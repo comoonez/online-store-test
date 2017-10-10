3b68d0406c1411be2e9771ebc2c5d10668971447
comoonez <32553164+comoonez@users.noreply.github.com>
2017-10-10 17:59:10 +0300

    Update CommonElements.java

--- CommonElements.java
+++ CommonElements.java
@@ -8,11 +8,11 @@ public class CommonElements {
 	@FindBy(xpath=".//*[@id='advertPopup' or @class='CountryRedirectModal modal in']//button[@class='close']")
 	public WebElement popUpCloseButton;
 	
-	public void ifPopupExistClose() {		//waiting for a pop-up window to be appeared and closing it
+	public void ifPopupExistClose() {		// waiting for a pop-up window to be appeared and closing it
 		try {
 			if(popUpCloseButton.isDisplayed());
 			popUpCloseButton.click();
-		} catch (Exception e) {
+		} catch (Exception e) {			// will throw exception if pop-up not found
 			System.out.println("Popup didn't appear.");
 		}
 	}	
